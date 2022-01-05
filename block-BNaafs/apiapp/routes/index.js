6e69357e089206bc80318ccf86ea796c8e7caa94
Sumit Chahar <chahar.sumit888@gmail.com>
2022-01-05 19:52:50 +0530

    sign and validate jwt's

---
+++ block-BNaafs/apiapp/routes/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var auth = require('../middlewares/auth');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/protected', auth.verifyToken, (req, res, next) => {
+  console.log(req.user);
+  res.status(201).json({ access: "protected resource" });
+})
+
+module.exports = router;
