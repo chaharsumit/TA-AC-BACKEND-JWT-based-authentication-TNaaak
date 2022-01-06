7fc83a3d382749153533eef8e978158505175369
Sumit Chahar <chahar.sumit888@gmail.com>
2022-01-06 13:30:57 +0530

    Add models and methods to generate jwt tokens

---
+++ block-BNaaen/conduit/models/User.js
@@ -0,0 +1,55 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  email: { type: String, required: true, unique: true },
+  username: String,
+  bio: String,
+  image: String,
+  password: { type: String, required: true },
+  following: [{ type: String }],
+  followers: [{ type: String }],
+  favouriteArticles: [{ type: String }]
+}, { timestamps: true });
+
+
+userSchema.pre("save", async function(next){
+  if(this.password && this.isModified("password")){
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  next();
+})
+
+userSchema.methods.verifyPassword = async function(password){
+  try{
+    let result = bcrypt.compare(password, this.password);
+    return result;
+  }catch(error){
+    return error;
+  }
+}
+
+userSchema.methods.signToken = async function(){
+  let payload = { userId: this.id, email: this.email };
+  try{
+    let token = await jwt.sign(payload, "thisisasecretstring");
+    return token;
+  }catch(error){
+    return error;
+  }
+}
+
+userSchema.methods.userJSON = async function(token){
+  return {
+    userId: this.id,
+    email: this.email,
+    token: token
+  }
+}
+
+mongoose.model('User', userSchema);
+
+module.exports = User;
